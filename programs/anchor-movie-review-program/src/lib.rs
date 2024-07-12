1e0f075102cfbbe0d722fbe0051657175410029b
AMALENDU MANOJ <90255760+amalendu123@users.noreply.github.com>
2024-07-12 15:45:56 +0530

    .

--- programs/anchor-movie-review-program/src/lib.rs
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -5,12 +5,103 @@ declare_id!("Dth2cjRA31z1SA5rossfiPtjCT3EKxJKFRytSvZHmsXV");
 #[program]
 pub mod anchor_movie_review_program {
     use super::*;
+    pub fn add_movie_review(
+        ctx: Context<AddMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie Review Account Created");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.reviewer = ctx.accounts.initializer.key();
+        movie_review.title = title;
+        movie_review.rating = rating;
+        movie_review.description = description;
         Ok(())
     }
+    pub fn update_movie_review(
+        ctx: Context<UpdateMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie review account space reallocated");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.rating = rating;
+        movie_review.description = description;
+
+        Ok(())
+    }
+    pub fn delete_movie_review(_ctx: Context<DeleteMovieReview>, title: String) -> Result<()> {
+        msg!("Movie review for {} deleted", title);
+        Ok(())
+    }
+
+   
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(title:String, description:String)]
+pub struct AddMovieReview<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = MovieAccountState::INIT_SPACE + title.len() + description.len(),
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct UpdateMovieReview<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc = MovieAccountState::INIT_SPACE + title.len() + description.len(),
+        realloc::payer = initializer,
+        realloc::zero = true,
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+pub struct MovieAccountState {
+    pub reviewer: Pubkey,
+    pub rating: u8,
+    pub title: String,
+    pub description: String,
+}
+impl Space for MovieAccountState {
+    const INIT_SPACE: usize = ANCHOR_DISCRIMINATOR + PUBKEY_SIZE + U8_SIZE + STRING_LENGTH_PREFIX + STRING_LENGTH_PREFIX;
+}
--- tests/anchor-movie-review-program.ts
+++ tests/anchor-movie-review-program.ts
@@ -1,16 +1,56 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program";
+import * as anchor from "@coral-xyz/anchor"
+import { Program } from "@coral-xyz/anchor"
+import { expect } from "chai"
+import { AnchorMovieReviewProgram } from "../target/types/anchor_movie_review_program"
 
 describe("anchor-movie-review-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
 
-  const program = anchor.workspace.AnchorMovieReviewProgram as Program<AnchorMovieReviewProgram>;
+  const program = anchor.workspace
+    .AnchorMovieReviewProgram as Program<AnchorMovieReviewProgram>
 
-  it("Is initialized!", async () => {
+  const movie = {
+    title: "Just a test movie",
+    description: "Wow what a good movie it was real great",
+    rating: 5,
+  }
+
+  const [moviePda] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from(movie.title), provider.wallet.publicKey.toBuffer()],
+    program.programId
+  )
+
+  it("Movie review is added`", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+    const tx = await program.methods
+      .addMovieReview(movie.title, movie.description, movie.rating)
+      .rpc()
+  
+    const account = await program.account.movieAccountState.fetch(moviePda)
+    expect(movie.title === account.title)
+    expect(movie.rating === account.rating)
+    expect(movie.description === account.description)
+    expect(account.reviewer === provider.wallet.publicKey)
+  })
+  it("Movie review is updated`", async () => {
+    const newDescription = "Wow this is new"
+    const newRating = 4
+  
+    const tx = await program.methods
+      .updateMovieReview(movie.title, newDescription, newRating)
+      .rpc()
+  
+    const account = await program.account.movieAccountState.fetch(moviePda)
+    expect(movie.title === account.title)
+    expect(newRating === account.rating)
+    expect(newDescription === account.description)
+    expect(account.reviewer === provider.wallet.publicKey)
+  })
+  it("Deletes a movie review", async () => {
+    const tx = await program.methods
+      .deleteMovieReview(movie.title)
+      .rpc()
+  })
+})
